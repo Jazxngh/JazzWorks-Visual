b62e85650026329559cde6c861f67d84caf11ec2
Jazxngh <Singhjaspinder528@gmail.com>
2025-05-20 22:02:29 +0100

    v1

--- server.js
+++ server.js
@@ -1,42 +1,43 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const app = express();
 const PORT = 3000;
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public")); 
-// The beauty of this is that this tells express, "use this as the root setup to run static stuff"
+app.use(express.static('public')); 
+//The beauty of this middleware is that this tells express, "use this as the root setup to run static stuff"
 //Meaning you DON'T need to public/image/xxx.jpg or public/style/desktop.css etc
 
-// Request form POST route
-app.post("/send", (req, res) => {
+// POST route for form submitting
+app.post('/send', (req, res) => {
   const { first, last, email, message } = req.body;
   const fullName = `${first} ${last}`;
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: 'gmail',
     auth: {
-      user: "singhjaspinder528@gmail.com",
-      pass: "SurajSingh14"
-      //My Gmail...
+      user: 'singhjaspinder528@gmail.com',
+      pass: 'qvwm uzco sxhi zqpk' 
+      //My Gmail & app password ... for security, app password temporary one time, if necessary can regenerate a new one in google account
     }
   });
 
   const mailOptions = {
     from: email,
-    to: "singhjaspinder528@gmail.com",
+    to: 'singhjaspinder528@gmail.com',
     subject: `Message from ${fullName}`,
     text: message
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.log(error);
-      res.send("error");
+      console.error(error);
+      res.send('error');
     } else {
-      console.log("Email sent: " + info.response);
-      res.send("success");
+      console.log('Email sent: ' + info.response);
+      res.send('success');
     }
   });
 });
@@ -44,3 +45,6 @@ app.post("/send", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+
